1e5f528e42a345c6c7622a3264314300cf72aa16
LoneWolf6383 <namasivaayam007vijay@gmail.com>
2022-08-24 22:04:39 +0530

    basic log in App

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      UserName:<input type='text' className='username' id='username'/>
+      Password:<input type='password' className='password' />
+      <input type="submit" value="submit" />
+    </div>
+  );
+}
+
+export default App;
