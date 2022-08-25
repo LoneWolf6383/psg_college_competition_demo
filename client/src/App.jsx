0284885b32bb6b575a6af865f5d63337f3ffc08b
Sweeton <sweeton2001@gmail.com>
2022-08-25 12:40:51 +0530

    Added Images Folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      UserName:<input type='text' className='username' id='username'/>
-      Password:<input type='password' className='password' />
-      <input type="submit" value="submit" />
+export function App(){
+  return(
+    <div className="login">
+      <h1>E COMMERCE WEBSITE</h1>
+      <a href="https://www.google.com/">Log In</a><br></br>
+      <a href="https://in.search.yahoo.com/?fr2=inr">Sign Up</a>
     </div>
   );
 }
 
-export default App;
