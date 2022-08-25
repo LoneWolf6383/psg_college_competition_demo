3e94ebbc1e177829712e138e46f4aa69e31dd15a
LoneWolf6383 <namasivaayam007vijay@gmail.com>
2022-08-25 16:59:24 +0530

    package update custom-env

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App/>}/>  
+          <Route path='/login' element={<LogIn/>}/>  
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
   </React.StrictMode>
 );
 
