63bb36e26c531f40183eae33fc160baf404fbc72
Sweeton <sweeton2001@gmail.com>
2022-08-25 17:31:57 +0530

    Final Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,19 @@
 import './App.css';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import {useState} from "react";
+import axios from "axios";
+import { DrawerAppBar} from './components/Navbar'
+
+export const LogInPage=()=> {
+  const[username,setUserName] = useState('')
+  const[password,setPassword] = useState('')
+  async function handleSignIn(event) {
+    event.preventDefault()
+      const data = { username, password}
+      const res = await axios.post(process.env.REACT_APP_NODEJS_URL+'/feedback/signin', data)
+  }
+}
 
 function App(){
   return(
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  AppBar,
+  Button,
+  Tab,
+  Tabs,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import DrawerComp from "./Drawer";
+const Navbar = () => {
+  const [value, setValue] = useState();
+  const theme = useTheme();
+  console.log(theme);
+  const isMatch = useMediaQuery(theme.breakpoints.down("md"));
+  console.log(isMatch);
 
-export const Navbar = () => {
   return (
-    <div>
-    
-    </div>
-  )
-}
+    <React.Fragment>
+      <AppBar>
+        <Toolbar>
+          <AddShoppingCartIcon sx={{ transform: "scale(1.5)" }} />
+          {isMatch ? (
+            <>
+              <Typography sx={{ fontSize: "1.2rem", paddingLeft: "10%" }}>
+                Troubleshooters Of MEPCO
+              </Typography>
+              <DrawerComp />
+            </>
+          ) : (
+            <>
+              <Tabs
+                sx={{ marginLeft: "auto" }}
+                indicatorColor="secondary"
+                textColor="inherit"
+                value={value}
+                onChange={(e, value) => setValue(value)}
+              >
+                <Tab label="Products" />
+                <Tab label="Services" />
+                <Tab label="About Us" />
+                <Tab label="Contact" />
+              </Tabs>
+              <Button sx={{ marginLeft: "auto" }} variant="contained">
+                Login
+              </Button>
+              <Button sx={{ marginLeft: "10px" }} variant="contained">
+                SignUp
+              </Button>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+};
+
+export default Navbar;
