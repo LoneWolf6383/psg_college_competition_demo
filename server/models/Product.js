2a860aeaa3cc9c80f1c62a8bde97dcff4dcbc3e1
GokulRames <96327033+Gokul05032002@users.noreply.github.com>
2022-08-26 12:51:42 +0530

    updated product.js

--- server/models/Product.js
+++ server/models/Product.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
+    productId:{type:String,required:true,unique:true},
     desc: { type: String, required: true, },
     img: { type: String, required: true },
     categories: { type: Array },
