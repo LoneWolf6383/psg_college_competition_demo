88f8a2262894c5021f149ac7a5dd8e24666ef778
LoneWolf6383 <namasivaayam007vijay@gmail.com>
2022-08-26 16:10:19 +0530

    Modified server

--- server/db.js
+++ server/db.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
-// const mongoUrl='mongodb://127.0.0.1:27017/PSG_iHackFest'
-const mongoUrl='mongodb+srv://Namasivaayam007:6383512055@cluster0.zaxrt3p.mongodb.net/?retryWrites=true&w=majority/PSG_iHackFest'
+const mongoUrl='mongodb://127.0.0.1:27017/PSG_iHackFest'
+// const mongoUrl='mongodb+srv://Namasivaayam007:6383512055@cluster0.zaxrt3p.mongodb.net/'
 
 module.exports = () => { 
     try {
-        mongoose.connect(mongoUrl, {
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        })
+        mongoose.connect(mongoUrl, {dbName:'PSG_iHackFest'})
         console.log('Connection successfull');
     } catch (error) {
         console.log(error,'failed');
--- server/routes/cart.js
+++ server/routes/cart.js
@@ -13,6 +13,7 @@ router.post("/", verifyToken, async (req, res) => {
   const newCart = new Cart(req.body);
 
   try {
+    console.log(req.body)
     const savedCart = await newCart.save();
     res.status(200).json(savedCart);
   } catch (err) {
@@ -23,6 +24,7 @@ router.post("/", verifyToken, async (req, res) => {
 //UPDATE
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
+    console.log(req.body)
     const updatedCart = await Cart.findByIdAndUpdate(
       req.params.id,
       {
@@ -39,6 +41,7 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
 //DELETE
 router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
+    console.log(req.params)
     await Cart.findByIdAndDelete(req.params.id);
     res.status(200).json("Cart has been deleted...");
   } catch (err) {
@@ -49,6 +52,7 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
 //GET USER CART
 router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
   try {
+    console.log(req.params)
     const cart = await Cart.findOne({ userId: req.params.userId });
     res.status(200).json(cart);
   } catch (err) {
@@ -57,9 +61,9 @@ router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
 });
 
 // //GET ALL
-
 router.get("/", verifyTokenAndAdmin, async (req, res) => {
   try {
+      console.log(req.body)
     const carts = await Cart.find();
     res.status(200).json(carts);
   } catch (err) {
