fab576120c226a6910a7bf207402c39449012751
Sweeton <sweeton2001@gmail.com>
2022-08-26 14:33:36 +0530

    Python Code

--- server/utilities/shopDeterminer.py
+++ server/utilities/shopDeterminer.py
@@ -13,7 +13,7 @@ shop_details=[
     },
     {
         'shopId':3,
-        'shopName':' Sweeton',
+        'shopName':'Sweeton',
         'products':['p3','p4','p6','p2','p8'],
         'location':[51,32]
     },
@@ -25,7 +25,7 @@ shop_details=[
     },
     {
         'shopId':5,
-        'shopName':' Gokul & co',
+        'shopName':'Gokul & co',
         'products':['p4','p2','p6','p6','p4'],
         'location':[3,5]
     },
@@ -57,13 +57,56 @@ all_products=[
     'p11',
 ]
 
-def findMinDistance(seller,customer):
-    dist=((int(seller[0])**2-int(customer[0]**2))+(seller[1]**2-customer[1]**2))
-    print(dist)
+dist_shop=[]
+dummy=[]
+dummy1=[]
+array1=[]
 
-user_req=input('Enter product Id:')
-user_location=list(input('Enter your location as (x,y)').split(','))
+def findMinDistance(seller,customer,name):
+    '''dist1=(int(seller[0])-int(customer[0]))**2
+    dist2=(int(seller[1])-int(customer[1]))**2
+    dist=int((dist2-dist1)**0.5)'''
+    
+    dist1=int(seller[0])-int(customer[0])
+    dist2=int(seller[1])-int(customer[1])
+    if(dist1<0):
+        dist1=dist1*-1
+        
+    if(dist2<0):
+        dist2=dist2*-1
+        
+    dist=dist1+dist2
+    
+    dist_shop.append([dist,name])
+
+user_req=input('Enter product Id\n')
+user_location=list(input('Enter your location as (x,y)\n').split(','))
 for i in shop_details:
     if user_req in i['products']:
-        dist = findMinDistance(i['location'],user_location)
-        print(dist)
+        dist = findMinDistance(i['location'],user_location,i['shopName'])
+
+def Sort(array):
+    array1.sort(key = lambda x: x[0])
+    return array
+    
+array = dist_shop
+array1=Sort(array)
+
+        
+for j in dist_shop:
+    dummy.append(j[0])
+    
+print("Available Shops And Distance\n")
+for i in array1:
+    print(i[1],"           ",i[0])
+    
+minimum=min(dummy)
+
+for i in dist_shop:
+   if (i[0]==minimum):
+       print("The Nearest Shop Is\n")
+       print(i[1])
+    
+for j in dummy:
+    dummy1.append(j)
+  
