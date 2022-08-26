78c3629b140a7b28d8c80b1f496996c69d57c6bf
LoneWolf6383 <namasivaayam007vijay@gmail.com>
2022-08-26 10:19:20 +0530

    added mongo url

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // const mongoUrl='mongodb://127.0.0.1:27017/PSG_iHackFest'
 const mongoUrl='mongodb+srv://Namasivaayam007:6383512055@cluster0.zaxrt3p.mongodb.net/?retryWrites=true&w=majority/PSG_iHackFest'
 
-module.exports = () => {
+module.exports = () => { 
     try {
         mongoose.connect(mongoUrl, {
             useNewUrlParser:true,
