06debf3b54517b9b59807f5e0b2e2df0ffbcbb60
siyon2003 <76099725+siyon2003@users.noreply.github.com>
2022-08-27 08:03:21 +0530

    title changes

--- client/src/data.js
+++ client/src/data.js
@@ -47,33 +47,41 @@ export const sliderItems = [
     {
       id:1,
       img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+      title:"T SHIRT"
     },
     {
       id:2,
       img:"https://m.media-amazon.com/images/I/A1vJUKBjc2L._CLa%7C2140%2C2000%7C51oS8vx9DkL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_UL1500_.png",
+      title:"ROUNDNECK SHIRT"
     },
     {
       id:3,
       img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+      title:"HOLIDAY SHIRT"
     },
     {
       id:4,
       img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+      title:"FROCKS"
     },
     {
       id:5,
       img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+      title:"HANDBAG"
     },
     {
       id:6,
       img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+      title:"CAP"
     },
     {
       id:7,
       img:"https://img.freepik.com/free-vector/color-sport-sneaker_98292-3191.jpg?w=900&t=st=1661532507~exp=1661533107~hmac=8d6efd169ec6f522c7154bd6b666d5e5ee33f2c0d15eacef9884731175bb638a",
+      title:"JORDAN SHOES"
     },
     {
       id:8,
       img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+      title:"JACKET"
     },
   ]
